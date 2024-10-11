cc87c94d10ee9a0c220b8ae5b7d8b178e7712683
yuyinws <lyc657508366@gmail.com>
2024-10-11 09:41:49 +0800

    fix: menu button show

--- app/components/HeaderBar.vue
+++ app/components/HeaderBar.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 const { isOpen } = storeToRefs(useSlide())
+const { status } = storeToRefs(useWebcontainerStore())
 </script>
 
 <template>
   <header class="bg-background/75 backdrop-blur border-b -mb-px sticky top-0 z-50 border-gray-200 dark:border-gray-800">
-    <div class="mx-auto px-6 flex items-center justify-between font-bold gap-3 h-14">
+    <div class="mx-auto px-4 sm:px-6 flex items-center justify-between font-bold gap-3 h-14">
       <UButton
+        v-show="status === 'finish'"
         icon="i-heroicons:bars-3-20-solid"
         aria-label="Menu"
         color="gray"
